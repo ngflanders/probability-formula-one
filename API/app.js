e174a689915cd12baf7bb3115c8e5c0cc12d66c5
Nicholas Flanders <ngflanders@gmail.com>
2018-10-01 22:42:47 -0500

    Added cors support

--- API/app.js
+++ API/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const resultsRouter = require('./routes/results');
@@ -11,6 +12,7 @@ const racesRouter = require('./routes/races');
 let app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
