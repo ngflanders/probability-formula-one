e4fce5600a3006b470162db6c525e9523b33b94f
Nicholas Flanders <ngflanders@gmail.com>
2018-10-07 20:34:12 -0500

    Wont hit the ergast api if it already gathered data from it this weekend

--- Database/Syncing/checkLastInsert.js
+++ Database/Syncing/checkLastInsert.js
@@ -1,5 +1,5 @@
 function checkLastInsert(db, table) {
-  let lastResultsWeHave = "select year, round from myf1db." + table + " join myf1db.races on races.raceId = " + table + ".raceId order by year desc, round desc limit 1;";
+  let lastResultsWeHave = "select year, round, races.date, races.time from myf1db." + table + " join myf1db.races on races.raceId = " + table + ".raceId order by year desc, round desc limit 1;";
   return new Promise(function (resolve, reject) {
     db.query(lastResultsWeHave, function (err, result) {
       if (err) {
--- Database/Syncing/main.js
+++ Database/Syncing/main.js
@@ -15,21 +15,29 @@ dbResults.connect(dbConnectionError);
 dbStandings.connect(dbConnectionError);
 dbConstructorStandings.connect();
 
+var today = new Date();
+var yesterday = new Date().setDate(today - 1);
+
 checkLastInsert.checkLastInsert(dbQuali, "qualifying").then(
     function (lastQuali) {
-      queryAPI.fetch(lastQuali.year, lastQuali.round+1, "qualifying").then(
-          function (qualiInfo) {
-            if (qualiInfo.total > 0) {
-              insertIntoDB.insertNewQualifying(dbQuali, qualiInfo.RaceTable.Races[0]);
-            } else {
-              util.noResults("qualifying", qualiInfo.RaceTable);
+      let lastInsertDate = new Date(lastQuali.date);
+      if (lastInsertDate.getDate() !== today.getDate() && lastInsertDate.getDate() !== yesterday.getDate()) {
+        queryAPI.fetch(lastQuali.year, lastQuali.round + 1, "qualifying").then(
+            function (qualiInfo) {
+              if (qualiInfo.total > 0) {
+                insertIntoDB.insertNewQualifying(dbQuali, qualiInfo.RaceTable.Races[0]);
+              } else {
+                util.noResults("qualifying", qualiInfo.RaceTable);
+                dbQuali.end();
+              }
+            }, function (err) {
+              console.error(err);
+              console.error("queryAPI.qualifying: rejected Promise")
               dbQuali.end();
-            }
-          }, function (err) {
-            console.error(err);
-            console.error("queryAPI.qualifying: rejected Promise")
-            dbQuali.end();
-          });
+            });
+      } else {
+        console.log(`${util.getDateTime()} - (qualifying): Already got this weekend's info.`);
+      }
     }, function (err) {
       console.error(err);
       console.error("checkLastInsert.qualifying: rejected Promise")
@@ -38,19 +46,24 @@ checkLastInsert.checkLastInsert(dbQuali, "qualifying").then(
 
 checkLastInsert.checkLastInsert(dbResults, "results").then(
     function (lastResult) {
-      queryAPI.fetch(lastResult.year, lastResult.round+1, "results").then(
-          function (raceInfo) {
-            if (raceInfo.total > 0) {
-              insertIntoDB.insertNewResults(dbResults, raceInfo.RaceTable.Races[0]); // inserts and ends dbConnection
-            } else {
-              util.noResults("results", raceInfo.RaceTable);
+      let lastResultDate = new Date(lastResult.date);
+      if (lastResultDate.getDate() !== today.getDate() && lastResultDate.getDate() !== yesterday.getDate()) {
+        queryAPI.fetch(lastResult.year, lastResult.round + 1, "results").then(
+            function (raceInfo) {
+              if (raceInfo.total > 0) {
+                insertIntoDB.insertNewResults(dbResults, raceInfo.RaceTable.Races[0]); // inserts and ends dbConnection
+              } else {
+                util.noResults("results", raceInfo.RaceTable);
+                dbResults.end();
+              }
+            }, function (err) {
+              console.error(err);
+              console.error("queryAPI.fetchRaceResults: rejected Promise")
               dbResults.end();
-            }
-          }, function (err) {
-            console.error(err);
-            console.error("queryAPI.fetchRaceResults: rejected Promise")
-            dbResults.end();
-          });
+            });
+      } else {
+        console.log(`${util.getDateTime()} - (results): Already got this weekend's info.`);
+      }
     }, function (err) {
       console.error(err);
       console.error("checkLastInsert.results: rejected Promise")
@@ -59,19 +72,24 @@ checkLastInsert.checkLastInsert(dbResults, "results").then(
 
 checkLastInsert.checkLastInsert(dbStandings, "driverstandings").then(
     function (lastStandings) {
-      queryAPI.fetch(lastStandings.year, lastStandings.round+1, "driverstandings").then(
-          function (standingsInfo) {
-            if (standingsInfo.total > 0) {
-              insertIntoDB.insertNewDriverStandings(dbStandings, standingsInfo.StandingsTable.StandingsLists[0]);
-            } else {
-              util.noResults("standings", standingsInfo.StandingsTable);
+      let lastStandingsDate = new Date(lastStandings.date);
+      if (lastStandingsDate.getDate() !== today.getDate() && lastStandingsDate.getDate() !== yesterday.getDate()) {
+        queryAPI.fetch(lastStandings.year, lastStandings.round + 1, "driverstandings").then(
+            function (standingsInfo) {
+              if (standingsInfo.total > 0) {
+                insertIntoDB.insertNewDriverStandings(dbStandings, standingsInfo.StandingsTable.StandingsLists[0]);
+              } else {
+                util.noResults("standings", standingsInfo.StandingsTable);
+                dbStandings.end();
+              }
+            }, function (err) {
+              console.error(err);
+              console.error("queryAPI.driverstandings: rejected Promise")
               dbStandings.end();
-            }
-          }, function (err) {
-            console.error(err);
-            console.error("queryAPI.driverstandings: rejected Promise")
-            dbStandings.end();
-          });
+            });
+      } else {
+        console.log(`${util.getDateTime()} - (driverstandings): Already got this weekend's info.`);
+      }
     },
     function (err) {
       console.error(err);
@@ -81,19 +99,24 @@ checkLastInsert.checkLastInsert(dbStandings, "driverstandings").then(
 
 checkLastInsert.checkLastInsert(dbConstructorStandings, "constructorstandings").then(
     function (lastStandings) {
-      queryAPI.fetch(lastStandings.year, lastStandings.round+1, "constructorStandings").then(
-          function (standingsInfo) {
-            if (standingsInfo.total > 0) {
-              insertIntoDB.insertNewConstructorStandings(dbConstructorStandings, standingsInfo.StandingsTable.StandingsLists[0]);
-            } else {
-              util.noResults("constructorstandings", standingsInfo.StandingsTable);
+      let lastStandingsDate = new Date(lastStandings.date);
+      if (lastStandingsDate.getDate() !== today.getDate() && lastStandingsDate.getDate() !== yesterday.getDate()) {
+        queryAPI.fetch(lastStandings.year, lastStandings.round + 1, "constructorStandings").then(
+            function (standingsInfo) {
+              if (standingsInfo.total > 0) {
+                insertIntoDB.insertNewConstructorStandings(dbConstructorStandings, standingsInfo.StandingsTable.StandingsLists[0]);
+              } else {
+                util.noResults("constructorstandings", standingsInfo.StandingsTable);
+                dbConstructorStandings.end();
+              }
+            }, function (err) {
+              console.error(err);
+              console.error("queryAPI.constructorStandings: rejected Promise")
               dbConstructorStandings.end();
-            }
-          }, function (err) {
-            console.error(err);
-            console.error("queryAPI.constructorStandings: rejected Promise")
-            dbConstructorStandings.end();
-          });
+            });
+      } else {
+        console.log(`${util.getDateTime()} - (constructorstandings): Already got this weekend's info.`);
+      }
     },
     function (err) {
       console.error(err);
@@ -103,5 +126,5 @@ checkLastInsert.checkLastInsert(dbConstructorStandings, "constructorstandings").
 
 
 function dbConnectionError(err) {
-    if (err) console.error(err);
+  if (err) console.error(err);
 }
