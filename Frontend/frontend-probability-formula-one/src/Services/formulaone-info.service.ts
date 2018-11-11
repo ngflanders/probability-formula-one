a119173f754a01d0fe6b46d96b6c68c95237a4f0
Nicholas Flanders <ngflanders@gmail.com>
2018-11-10 21:21:55 -0600

    Fix Api route for realz now

--- Frontend/frontend-probability-formula-one/src/Services/formulaone-info.service.ts
+++ Frontend/frontend-probability-formula-one/src/Services/formulaone-info.service.ts
@@ -5,29 +5,30 @@ import {Observable} from 'rxjs';
 @Injectable()
 export class FormulaoneInfoService {
 
+  baseAPI_url = "https://api.f1wiz.com";
   constructor(private http: HttpClient) { }
 
   getRaces(year: number, round?: number): Observable<any> {
-    return this.http.get(`https://localhost:6001/races/${year}/${round || ''}`);
+    return this.http.get(`${this.baseAPI_url}/races/${year}/${round || ''}`);
   }
 
   getQualifying(year: number, round?: number): Observable<any> {
-    return this.http.get(`https://localhost:6001/qualifying/${year}/${round || ''}`);
+    return this.http.get(`${this.baseAPI_url}/qualifying/${year}/${round || ''}`);
   }
 
   getRaceResults(year: number, round?: number): Observable<any> {
-    return this.http.get<any>(`https://localhost:6001/results/${year}/${round || ''}`);
+    return this.http.get<any>(`${this.baseAPI_url}/results/${year}/${round || ''}`);
   }
 
   getSimulatedFinalStandings(year: number, round?: number): Observable<any> {
-    return this.http.get<any>(`https://localhost:6001/simulations/${year}/${round || ''}`);
+    return this.http.get<any>(`${this.baseAPI_url}/simulations/${year}/${round || ''}`);
   }
 
   getDriverStandings(year:number,round?: number): Observable<any> {
-    return this.http.get<any>(`https://localhost:6001/standings/drivers/${year}/${round || ''}`);
+    return this.http.get<any>(`${this.baseAPI_url}/standings/drivers/${year}/${round || ''}`);
   }
 
   getConstructorStandings(year:number,round?: number): Observable<any> {
-    return this.http.get<any>(`https://localhost:6001/standings/constructors/${year}/${round || ''}`);
+    return this.http.get<any>(`${this.baseAPI_url}/standings/constructors/${year}/${round || ''}`);
   }
 }
