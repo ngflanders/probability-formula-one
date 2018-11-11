b8571df4c28751cfe64d39a935595dfc0fd639f8
Nicholas Flanders <ngflanders@gmail.com>
2018-11-10 21:36:48 -0600

    Highlight selected driver bug fix

--- Frontend/frontend-probability-formula-one/src/app/season-results/season-results.component.ts
+++ Frontend/frontend-probability-formula-one/src/app/season-results/season-results.component.ts
@@ -42,9 +42,7 @@ export class SeasonResultsComponent implements OnInit {
   };
 
   onClickHighlightProgress(evt) {
-    console.log(evt);
     this.clickedDriver = (this.clickedDriver !== evt.target.innerText) ? evt.target.innerText.trim() : "";
-    console.log(this.clickedDriver);
   }
 
 }
